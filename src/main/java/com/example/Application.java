fba0ef4e68408e28483e7a570a921b301bb7d161
mikhail <dolgopeh@gmail.com>
2020-01-24 20:49:29 +0300

    ++

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,8 +1,17 @@
 package com.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Application {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
         Data data = new Data("важные данные", "2451");
         System.out.println(data);
+        LOGGER.error("Error");
+        LOGGER.info("Some info");
+        LOGGER.warn("Warning");
     }
 }
